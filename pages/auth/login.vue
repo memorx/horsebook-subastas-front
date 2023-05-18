e32362a47475eedc5122338951ace01253eb983f
matiaskoziupa <matikoziupa@gmail.com>
2023-05-18 13:29:01 -0300

    feat(hardcoded-token): Hardcoded the .env file to test the app

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -306,9 +306,9 @@ export default {
     async userLogin() {
       // Login the user
       const url = this.$config.baseURL + '/users/login-app/'
-      const token = "Token 4fd2e979427a259cc56c18cad449cec5aefaed0d"
       const headers = {
-        Authorization: "Token " + process.env.TOKEN,
+        Authorization: "Token 0119158e9e647cc58e9c895fa08316b2a5b03df4",
+        // Authorization: "Token " + process.env.TOKEN,
       };
       const formData = new FormData()
       formData.append('email', this.login.email)
