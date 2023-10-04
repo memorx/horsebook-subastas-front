747418cff792b8c0b8d8b9536d6f27b086969f08
ReneAlluxi <rene.ramos@alluxi.com>
2023-10-04 11:26:54 -0600

    Multiple Fixes

--- components/bid/modal.vue
+++ components/bid/modal.vue
@@ -1,67 +1,71 @@
 <template>
-    <div class="modal fixed inset-0 flex items-center justify-center z-50">
-        <div class="bg-white rounded-lg p-6 shadow-xl text-center">
-            <h2 class="text-lg font-bold">¿Estas seguro que quieres ofertar?</h2>
-            <p class="text-3xl font-bold">$ {{ amount }} USD</p>
-            <div class="mt-4 flex justify-center">
-                <button
-                    class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg mr-4 transition duration-300 ease-in-out"
-                    @click="confirm"
-                >
-                    Confirmar
-                </button>
-                <button
-                    class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out"
-                    @click="disableModal"
-                >
-                    Cerrar
-                </button>
-            </div>
-        </div>
+  <div class="modal fixed inset-0 flex items-center justify-center z-50">
+    <div class="bg-white rounded-lg p-6 shadow-xl text-center">
+      <h2 v-if="status == 'BIDDING'" class="text-lg font-bold">
+        ¿Estas seguro que quieres ofertar?
+      </h2>
+      <h2 v-else class="text-lg font-bold">
+        ¿Estas seguro que quieres pre ofertar?
+      </h2>
+      <p class="text-3xl font-bold">$ {{ amount }} USD</p>
+      <div class="mt-4 flex justify-center">
+        <button
+          class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-lg mr-4 transition duration-300 ease-in-out"
+          @click="confirm"
+        >
+          Confirmar
+        </button>
+        <button
+          class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out"
+          @click="disableModal"
+        >
+          Cerrar
+        </button>
+      </div>
     </div>
-</template>  
-  
+  </div>
+</template>
+
 <script>
 export default {
-    props: {
-        amount: '',
-        submitForm: { type: Function },
-        disableModal: { type: Function },
+  props: {
+    amount: "",
+    submitForm: { type: Function },
+    disableModal: { type: Function },
+    status: ""
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    open() {
+      this.showModal = true
     },
-    data() {
-        return {
-        };
+    close() {
+      this.showModal = false
     },
-    methods: {
-        open() {
-            this.showModal = true;
-        },
-        close() {
-            this.showModal = false;
-        },
-        confirm(event) {
-            this.submitForm(event)
-            this.disableModal()
-        },
-    },
-};
+    confirm(event) {
+      this.submitForm(event)
+      this.disableModal()
+    }
+  }
+}
 </script>
-  
+
 <style scoped>
 /* Add your modal styling here */
 .modal {
-    background-color: rgba(128, 128, 128, 0.546);
+  background-color: rgba(128, 128, 128, 0.546);
 }
 
 .modal-content {
-    background-color: #fff;
-    margin: 0 auto;
-    text-align: center;
-    position: relative;
+  background-color: #fff;
+  margin: 0 auto;
+  text-align: center;
+  position: relative;
 }
 
 button {
-    margin-top: 10px;
+  margin-top: 10px;
 }
 </style>
-  
--- pages/user/detalles/_id.vue
+++ pages/user/detalles/_id.vue
@@ -5,7 +5,9 @@
       class="fixed w-full h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50"
     />
     <NuxtLink :to="`/user/inicio/`">
-      <button class="bg-gray-500 text-white px-4 py-2 rounded-md mx-3 mb-5">Atras</button>
+      <button class="bg-gray-500 text-white px-4 py-2 rounded-md mx-3 mb-5">
+        Atras
+      </button>
     </NuxtLink>
     <div class="flex flex-wrap">
       <!-- First Column -->
@@ -13,75 +15,108 @@
         <img
           src="../../../public/image_detail_horse.png"
           alt="logo"
-          class=" hidden md:block w-full max-w-full object-cover rounded-lg md:flex-grow"
+          class="hidden md:block w-full max-w-full object-cover rounded-lg md:flex-grow"
         />
       </div>
       <!-- Second Column -->
       <div class="w-full sm:w-1/2 md:flex md:flex-col">
         <!-- First Row in the second column -->
         <div class="mb-4 bg-white p-5 mx-5 rounded-lg">
-          <h2 class="text-2xl font-bold mb-1">Subasta la Silla</h2>
+          <div class="flex items-center">
+            <h2 class="text-2xl font-bold mb-1 mr-3">Subasta</h2>
+            <statusBid :status="bidStatus" />
+          </div>
           <h4 class="text-sm mb-4">🇲🇽 Monterrey, Nuevo Leon</h4>
-          <span class="text-xl font-bold mb-2 mr-1">{{ item.horses.length }}</span>
-          <span>{{ item.horses.length == 1 ? 'Caballo' : 'Caballos' }}</span>
+          <span class="text-xl font-bold mb-2 mr-1">{{
+            item.horses.length
+          }}</span>
+          <span>{{ item.horses.length == 1 ? "Caballo" : "Caballos" }}</span>
         </div>
-
         <!-- Second Row in the second column -->
         <div class="bg-white p-5 mx-5 rounded-lg md:flex-grow">
           <!-- PREOFERTA -->
           <h1
             v-if="bidStatus == 'COMING'"
             class="text-center text-sm font-bold"
-          >LA PRE OFERTA COMIENZA EN</h1>
+          >
+            LA PRE OFERTA COMIENZA EN
+          </h1>
           <h1
             v-if="bidStatus == 'PREBID'"
             class="text-center text-sm font-bold"
-          >LA PRE OFERTA HA COMENZADO</h1>
+          >
+            LA PRE OFERTA HA COMENZADO
+          </h1>
           <h1
             v-if="bidStatus == 'CLOSED PREBID'"
             class="text-center text-sm font-bold"
-          >LA PRE OFERTA HA TERMINADO Y</h1>
+          >
+            LA PRE OFERTA HA TERMINADO Y
+          </h1>
           <div class="flex justify-center">
             <div
               class="mx-10 my-10"
               v-if="countdownPre !== 'La Pre Oferta ha terminado'"
             >
-              <div class="md:flex md:items-center">
+              <div
+                v-if="
+                  preBidTime.days &&
+                  preBidTime.hours &&
+                  preBidTime.minutes &&
+                  preBidTime.seconds
+                "
+                class="md:flex md:items-center"
+              >
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ preBidTime.days }}</p>
-                  <p class="text-center text-slate-500">Days</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ preBidTime.days }}
+                  </p>
+                  <p class="text-center text-slate-500">Días</p>
                 </div>
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ preBidTime.hours }}</p>
-                  <p class="text-center text-slate-500">Hours</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ preBidTime.hours }}
+                  </p>
+                  <p class="text-center text-slate-500">Horas</p>
                 </div>
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ preBidTime.minutes }}</p>
-                  <p class="text-center text-slate-500">Minutes</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ preBidTime.minutes }}
+                  </p>
+                  <p class="text-center text-slate-500">Minutos</p>
                 </div>
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ preBidTime.seconds }}</p>
-                  <p class="text-center text-slate-500">Seconds</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ preBidTime.seconds }}
+                  </p>
+                  <p class="text-center text-slate-500">Segundos</p>
                 </div>
               </div>
-            </div>
-            <div v-if="bidStatus == 'PREBID'">
-              <div class="my-10 px-5 py-2 bg-green-600 text-white font-bold rounded-full">Abierta</div>
             </div>
           </div>
           <!-- SUBASTA -->
           <h1
             v-if="bidStatus == 'CLOSED'"
             class="text-center text-sm font-bold"
-          >LA SUBASTA HA TERMINADO</h1>
+          >
+            LA SUBASTA HA TERMINADO
+          </h1>
           <h1
-            v-if="bidStatus == 'CLOSED PREBID' || bidStatus == 'COMING'"
+            v-if="
+              bidStatus == 'CLOSED PREBID' ||
+              bidStatus == 'COMING' ||
+              bidStatus == 'PREBID'
+            "
             class="text-center text-sm font-bold"
-          >LA SUBASTA ESTARA EN VIVO EN</h1>
+          >
+            LA SUBASTA ESTARA EN VIVO EN
+          </h1>
           <h1
             v-if="bidStatus == 'BIDDING'"
             class="text-center text-sm font-bold"
-          >LA SUBASTA ESTA EN VIVO</h1>
+          >
+            LA SUBASTA ESTA EN VIVO
+          </h1>
           <div class="flex justify-center">
             <div
               class="mx-10 md:mx-0 my-10"
@@ -89,88 +124,93 @@
             >
               <div class="md:flex md:items-center">
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ bidTime.days }}</p>
-                  <p class="text-center text-slate-500">Days</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ bidTime.days }}
+                  </p>
+                  <p class="text-center text-slate-500">Días</p>
                 </div>
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ bidTime.hours }}</p>
-                  <p class="text-center text-slate-500">Hours</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ bidTime.hours }}
+                  </p>
+                  <p class="text-center text-slate-500">Horas</p>
                 </div>
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ bidTime.minutes }}</p>
-                  <p class="text-center text-slate-500">Minutes</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ bidTime.minutes }}
+                  </p>
+                  <p class="text-center text-slate-500">Minutos</p>
                 </div>
                 <div class="mx-5">
-                  <p class="text-center text-5xl mb-2 font-bold">{{ bidTime.seconds }}</p>
-                  <p class="text-center text-slate-500">Seconds</p>
+                  <p class="text-center text-5xl mb-2 font-bold">
+                    {{ bidTime.seconds }}
+                  </p>
+                  <p class="text-center text-slate-500">Segundos</p>
                 </div>
               </div>
             </div>
-            <!-- <div
-              v-if="bidStatus == 'BIDDING'"
-              class="w-full md:w-auto"
-            >
-              <button class="w-full my-10 py-3 px-5 bg-black text-white rounded-lg">INGRESA A LA SUBASATA</button>
-            </div> -->
           </div>
         </div>
       </div>
     </div>
     <div class="text-center md:text-left">
       <h1 class="text-4xl font-semibold my-6">Caballos a subastar</h1>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+      <div
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4"
+      >
         <div
           class="mx-5 md:mx-0 bg-white rounded-lg shadow-md"
           v-for="(horse, index) in item.horses"
           :key="horse.id"
         >
-          <div v-if="bidStatus == 'PREBID'">
-            <div
-              v-if="horse.local_data.status == 'COMING'"
-              class="text-center bg-yellow-300 mx-5 my-3 px-3 py-1 text-white font-bold text-sm rounded-full"
-            >PRE OFERTA</div>
-          </div>
-          <div v-else>
-            <div
-              v-if="horse.local_data.status == 'BIDDING'"
-              class="text-center bg-green-400 mx-5 my-3 px-3 py-1 text-white font-bold text-sm rounded-full"
-            >EN
-              VIVO
-            </div>
-            <div
-              v-if="horse.local_data.status == 'COMING'"
-              class="text-center bg-yellow-300 mx-5 my-3 px-3 py-1 text-white font-bold text-sm rounded-full"
-            >PROXIMA SUBASTA</div>
-            <div
-              v-if="horse.local_data.status == 'CLOSED'"
-              class="text-center bg-red-400 mx-5 my-3 px-3 py-1 text-white font-bold text-sm rounded-full"
-            >CERRADA
-            </div>
-          </div>
           <NuxtLink
             class="buttonDetails"
             :to="`/bids/bid?id=${id}&horsePositionList=${index}&horseId=${horse.local_data.id}`"
           >
-            <!-- Display the first image if it's available -->
+            <div class="px-1 py-1">
+              <horseStatus :status="horse.local_data.status" />
+            </div>
             <img
               v-if="getImageUrl(horse.local_data.horse_id)"
               :src="getImageUrl(horse.local_data.horse_id)"
               class="w-full object-cover"
-              style="height: 40vh;"
-            >
+              style="height: 40vh"
+            />
             <img
               v-else
               src="../../../public/horse_black.png"
               alt="Default Horse"
               class="w-full object-cover"
-              style="height: 40vh;"
-            >
-            <!-- <p class="text-sm  my-4 mx-6 text-slate-500">Canatra x Laval</p> -->
-            <p class="text-xl font-bold my-2 mx-6">{{ horse.external_data.name }}</p>
-            <p class="text-sm  my-2 mx-6 text-slate-500">2018</p>
+              style="height: 40vh"
+            />
+            <p class="text-xl font-bold my-2 mx-6">
+              {{ horse.external_data.name }}
+            </p>
             <div class="border-b border-gray-300 my-4 mx-5"></div>
-            <p class="text-sm font-bold text-center my-2 mx-3">Precio inicial</p>
-            <p class="text-lg font-bold text-center mt-2 mb-3 mx-3">$ {{ horse.local_data.final_amount }} USD</p>
+            <p
+              v-if="
+                amountStringToInt(
+                  horse.local_data.initial_pre_bid_amount,
+                  horse.local_data.final_amount
+                )
+              "
+              class="text-sm font-bold text-center my-2 mx-3"
+            >
+              Mejor Oferta
+            </p>
+            <p v-else class="text-sm font-bold text-center my-2 mx-3">
+              Precio Inicial
+            </p>
+            <p class="text-lg font-bold text-center mt-2 mb-3 mx-3">
+              $ {{ horse.local_data.final_amount }} USD
+            </p>
+            <div class="flex justify-center">
+              <button
+                class="bg-black rounded-lg px-4 py-2 text-white text-center mb-3 hover:bg-gray-800"
+              >
+                Ingresar
+              </button>
+            </div>
           </NuxtLink>
         </div>
       </div>
@@ -179,39 +219,41 @@
 </template>
 
 <script>
-import axios from 'axios'
-import Loading from '../../../components/shared/Loading.vue';
+import axios from "axios"
+import Loading from "../../../components/shared/Loading.vue"
+import statusBid from "../../../components/bid/statusBid.vue"
+import horseStatus from "../../../components/bid/horseStatus.vue"
 
 export default {
-  components: { Loading, },
+  components: { Loading, statusBid, horseStatus },
   data() {
     return {
       bidTime: {
-        days: '',
-        hours: '',
-        minutes: '',
-        seconds: '',
+        days: "",
+        hours: "",
+        minutes: "",
+        seconds: ""
       },
       preBidTime: {
-        days: '',
-        hours: '',
-        minutes: '',
-        seconds: '',
+        days: "",
+        hours: "",
+        minutes: "",
+        seconds: ""
       },
       item: {
-        horses: '',
-        start_bid: '',
-        start_pre_bid: '',
+        horses: "",
+        start_bid: "",
+        start_pre_bid: ""
       },
       horseData: {
         imagesObject: {},
-        imagesID: [],
+        imagesID: []
       },
-      id: '',
+      id: "",
       loading: false,
-      countdownSubasta: 'Calculando cuenta regresiva...',
-      countdownPre: 'Calculando cuenta regresiva...',
-      bidStatus: '',
+      countdownSubasta: "Calculando cuenta regresiva...",
+      countdownPre: "Calculando cuenta regresiva...",
+      bidStatus: ""
     }
   },
   async created() {
@@ -221,56 +263,67 @@ export default {
   },
   mounted() {
     // Update the countdown every second
-    this.timer = setInterval(this.calculateCountdown, 1000);
-    this.timer2 = setInterval(this.calculateCountdownPre, 1000);
+    this.timer = setInterval(this.calculateCountdown, 1000)
+    this.timer2 = setInterval(this.calculateCountdownPre, 1000)
   },
   beforeDestroy() {
     // Clear the timer when the component is destroyed
-    clearInterval(this.timer);
-    clearInterval(this.timer2);
+    clearInterval(this.timer)
+    clearInterval(this.timer2)
   },
   methods: {
+    amountStringToInt(initial, final) {
+      if (parseInt(final, 10) > parseInt(initial, 10)) {
+        return true
+      } else {
+        return false
+      }
+    },
     getImageUrl(horseID) {
-      return this.horseData.imagesObject[horseID];
+      return this.horseData.imagesObject[horseID]
     },
     fetchHorseImages(horseID) {
       if (!horseID) {
-        return; // Exit early if horseID is not defined
+        return // Exit early if horseID is not defined
       }
       axios
         .get(this.$config.baseLaSilla + `/horses/${horseID}/images`)
-        .then(response => {
+        .then((response) => {
           // Check if there are images in the response
           if (Array.isArray(response.data) && response.data.length > 0) {
-            const imageUrl = response.data[0].url;
-            this.$set(this.horseData.imagesObject, horseID, imageUrl);
+            const imageUrl = response.data[0].url
+            this.$set(this.horseData.imagesObject, horseID, imageUrl)
           } else {
-            console.warn('No images found in the response for horse: ', horseID);
+            console.warn("No images found in the response for horse: ", horseID)
           }
         })
-        .catch(error => {
-          console.error(error);
-        });
+        .catch((error) => {
+          console.error(error)
+        })
     },
 
     fetchImagesForAllHorses() {
-      this.horseData.imagesID.forEach(horseID => {
-        this.fetchHorseImages(horseID);
-      });
+      this.horseData.imagesID.forEach((horseID) => {
+        this.fetchHorseImages(horseID)
+      })
     },
     calculateCountdown() {
-      const now = new Date();
-      const targetDate = new Date(this.item.start_bid);
-      const timeDifference = targetDate - now;
+      const now = new Date()
+      const targetDate = new Date(this.item.start_bid)
+      const timeDifference = targetDate - now
 
       if (timeDifference <= 0) {
-        this.countdownSubasta = 'La Subasta ha terminado';
-        clearInterval(this.timer);
+        this.countdownSubasta = "La Subasta ha terminado"
+        clearInterval(this.timer)
       } else {
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+        const hours = Math.floor(
+          (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        )
+        const minutes = Math.floor(
+          (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+        )
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
         this.bidTime.days = days
         this.bidTime.hours = hours
         this.bidTime.minutes = minutes
@@ -278,18 +331,22 @@ export default {
       }
     },
     calculateCountdownPre() {
-      const now = new Date();
-      const targetDate = new Date(this.item.start_pre_bid);
-      const timeDifference = targetDate - now;
+      const now = new Date()
+      const targetDate = new Date(this.item.start_pre_bid)
+      const timeDifference = targetDate - now
 
       if (timeDifference <= 0) {
-        this.countdownPre = 'La Pre Oferta ha terminado';
-        clearInterval(this.timer2);
+        this.countdownPre = "La Pre Oferta ha terminado"
+        clearInterval(this.timer2)
       } else {
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+        const hours = Math.floor(
+          (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        )
+        const minutes = Math.floor(
+          (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+        )
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
         this.preBidTime.days = days
         this.preBidTime.hours = hours
         this.preBidTime.minutes = minutes
@@ -298,7 +355,7 @@ export default {
     },
     async getDetailsAuction(itemId) {
       const url = this.$config.baseURL + `/subastas/list-subastas/?id=${itemId}`
-      this.loading = true;
+      this.loading = true
 
       await this.$axios
         .get(url)
@@ -308,11 +365,13 @@ export default {
           this.item.horses = response.data.horses
           this.bidStatus = response.data.status
           console.log(this.bidStatus)
-          this.horseData.imagesID = response.data.horses.map(horse => horse.local_data.horse_id);
-          this.calculateCountdown()
-          this.calculateCountdownPre()
+          this.horseData.imagesID = response.data.horses.map(
+            (horse) => horse.local_data.horse_id
+          )
           this.fetchImagesForAllHorses()
           this.loading = false
+          this.calculateCountdown()
+          this.calculateCountdownPre()
         })
         .catch((error) => {
           console.log(error)
@@ -320,7 +379,7 @@ export default {
         })
     },
     showHorseDetails(horse) {
-      this.$set(horse, 'showDetails', !horse.showDetails);
+      this.$set(horse, "showDetails", !horse.showDetails)
     }
   }
 }
