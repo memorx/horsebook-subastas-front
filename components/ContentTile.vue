6962b384b7628a381efde654b644f8f91bc39f04
santiagoquintero95 <santiago.quintero@alluxi.com>
2023-10-19 17:52:09 -0500

    news section

--- components/ContentTile.vue
+++ components/ContentTile.vue
@@ -1,10 +1,12 @@
 <template>
-    <div :style="computedStyles.container" class="p-4 w-11/12 sm:w-4/5 rounded-md">
-        <component :is="`h${headingLevel}`" :style="computedStyles.title" class="text-xl sm:text-2xl font-bold">{{ title }}
+    <div :class="['p-4 w-11/12 sm:w-4/5 rounded-md', classOverrides.container || '']">
+        <component :is="`h${headingLevel}`" :class="['font-bold', classOverrides.title || '']">{{ title }}
         </component>
-        <p :style="computedStyles.paragraph">{{ paragraph }}</p>
-        <button v-if="buttonLabel" @click="buttonAction" :style="computedStyles.button"
-            class="mt-4 px-4 py-2 bg-black text-white rounded-2xl">{{ buttonLabel }}</button>
+        <p :class="[classOverrides.paragraph || '']">{{ paragraph }}</p>
+        <button v-if="buttonLabel" @click="buttonAction"
+            :class="['mt-4 px-4 py-2 bg-black text-white rounded-2xl', classOverrides.button || '']">
+            {{ buttonLabel }}
+        </button>
     </div>
 </template>
 
@@ -31,37 +33,9 @@ export default {
                 return num >= 1 && num <= 6;
             }
         },
-        styles: {
+        classOverrides: {  // Updated prop
             type: Object,
             default: () => ({})
-        }
-    },
-    computed: {
-        defaultStyles() {
-            return {
-                container: {
-                    backgroundColor: 'transparent'
-                },
-                title: {
-                    color: '#333'
-                },
-                paragraph: {
-                    color: '#555'
-                },
-                button: {
-                    backgroundColor: 'black',
-                    color: 'white'
-                }
-                // ... any other default styles you want
-            };
-        },
-        computedStyles() {
-            const styles = { ...this.defaultStyles };
-            if (this.styles.container) styles.container = { ...styles.container, ...this.styles.container };
-            if (this.styles.title) styles.title = { ...styles.title, ...this.styles.title };
-            if (this.styles.paragraph) styles.paragraph = { ...styles.paragraph, ...this.styles.paragraph };
-            if (this.styles.button) styles.button = { ...styles.button, ...this.styles.button };
-            return styles;
         }
     },
     methods: {
--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,33 @@
          <div
             class="w-full flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0 md:space-x-4 my-12 md:my-6">
             <div class="w-4/5 md:w-2/5 flex flex-col space-y-4 md:space-y-0">
-               <ContentTile :title="$t('home.main.title')" :paragraph="$t('home.main.paragraph')"
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
                   :buttonLabel="$t('home.main.button') ? $t('home.main.button') : null"
-                  @button-clicked="handleButtonClick(1)" headingLevel="1" :styles="{
-                     container: {},
-                     title: { color: 'white' },
-                     paragraph: { color: 'white' },
-                     button: { backgroundColor: '#D9D957', rounded: 'rounded' }
-                  }" />
+                  @button-clicked="handleButtonClick(1)"
+                  headingLevel="1"
+                  :classOverrides="{
+                     title: 'text-white text-xl',
+                     paragraph: 'text-white ',
+                     button: 'bg-yellow-300 rounded'  // This assumes that `#D9D957` is approximately equivalent to `bg-yellow-300` in Tailwind's colors
+                  }"
+               />
             </div>
 
             <div class="w-4/5 md:w-2/5 flex flex-col space-y-4 md:space-y-0">
-               <ContentTile :title="$t('home.main.title')" :paragraph="$t('home.main.paragraph')"
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
                   :buttonLabel="$t('home.main.button') ? $t('home.main.button') : null"
-                  @button-clicked="handleButtonClick(1)" headingLevel="2" :styles="{
-                     container: {},
-                     title: { color: 'white' },
-                     paragraph: { color: 'white' },
-                     button: { backgroundColor: '#D9D957', rounded: 'rounded' }
-                  }" />
+                  @button-clicked="handleButtonClick(1)"
+                  headingLevel="1"
+                  :classOverrides="{
+                     title: 'text-white text-xl',
+                     paragraph: 'text-white ',
+                     button: 'bg-yellow-300 rounded'  // This assumes that `#D9D957` is approximately equivalent to `bg-yellow-300` in Tailwind's colors
+                  }"
+               />
             </div>
          </div>
 
@@ -31,15 +39,93 @@
             titleClass="text-white text-4xl md:text-7xl lg:text-8xl font-bold uppercase" />
 
          <!-- next component here -->
-         <!-- <div class="w-full h-screen bg-white flex flex-row">
-            <div class="w-1/2 h-full bg-yellow-500 flex flex-col">
-               <div class="w-full h-1/2 bg-red-500 "></div>
-               <div class="w-full h-1/2 bg-green-500 "></div>
+         <!-- <div class="w-full h-screen flex flex-row">
+            <div class="w-1/2 h-full flex flex-col">
+               <div class="w-full h-2/3">
+                  <div class="w-3/4 h-full bg-yellow-500 ">
+
+                  </div>
+               </div>
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
+                  headingLevel="2" :styles="{
+                     container: {padding: '0 0 0 0'},
+                     title: { color: 'white' },
+                     paragraph: { color: 'white' },
+                  }" />
             </div>
-            <div class="w-1/2 h-full bg-blue-500 flex flex-col justify-center items-center">
-               <div class="w-full h-1/2 bg-red-500 "></div>
+            <div class="w-1/2 h-full flex flex-col items-start justify-start">
+               <div class="w-full h-2/3">
+                  <div class="w-3/4 h-full bg-yellow-500 ">
+                  </div>
+               </div>
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
+                  headingLevel="2" :styles="{
+                     container: {padding: '0 0 0 0'},
+                     title: { color: 'white' },
+                     paragraph: { color: 'white' },
+                  }" />
             </div>
          </div> -->
+         <!-- Replaced Grid -->
+         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 h-screen lg:h-[900px] w-full">
+            <div class=" p-4 flex flex-col items-start justify-center">
+               <div class="w-full h-2/3 flex ">
+                  <div class="lg:w-2/3 w-1/2 h-full border-yellow-500 border-1 rounded-tl-xl rounded-br-xl">
+                  </div>
+               </div>
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
+                  :buttonLabel="null"
+                  headingLevel="2"
+                  :classOverrides="{
+                     container: 'p-0 w-2/3',
+                     title: 'text-white text-base',
+                     paragraph: 'text-white',
+                  }"
+               />
+            </div>
+            <div class=" p-4 flex flex-col items-start justify-center">
+               <div class="w-full h-2/3 flex ">
+                  <div class="lg:w-2/3 w-1/2 h-full border-yellow-500 border-1 rounded-tl-xl rounded-br-xl">
+                  </div>
+               </div>
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
+                  :buttonLabel="null"
+                  headingLevel="2"
+                  :classOverrides="{
+                     container: 'p-0 w-2/3',
+                     title: 'text-white text-base',
+                     paragraph: 'text-white',
+                  }"
+               />
+            </div>
+            <div class=" p-4 flex flex-col items-start justify-center">
+               <div class="w-full h-2/3 flex ">
+                  <div class="lg:w-2/3 w-1/2 h-full border-yellow-500 border-1 rounded-tl-xl rounded-br-xl">
+                  </div>
+               </div>
+               <ContentTile
+                  :title="$t('home.main.title')"
+                  :paragraph="$t('home.main.paragraph')"
+                  :buttonLabel="null"
+                  headingLevel="2"
+                  :classOverrides="{
+                     container: 'p-0 w-2/3',
+                     title: 'text-white text-base',
+                     paragraph: 'text-white',
+                  }"
+               />
+            </div>
+            <!-- <div class="bg-gray-300 p-4 flex flex-col items-start justify-center">Item 4</div> -->
+         </div>
+         <!-- end of replaced grid -->
 
 
          <SectionTitle :titleText="$t('home.news.title')" containerClass="w-full my-0 mb-8"
