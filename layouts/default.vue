7e453a3c4487659b4f7e42c3c6f528557d09a193
Suzana Salcedo <suzana@alluxi.com>
2023-10-31 16:17:11 -0600

    Browser update notice & Link fix for horses

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,14 +7,25 @@
         <Nuxt />
     </div>
 </template>
-  
+
 <script>
 import Topbar from '~/components/Topbar.vue';
 
 export default {
     components: {
         Topbar
-    }
+    },
+    mounted() {
+        if (!window.WebSocket) {
+            this.$swal.fire({
+                title: 'Actualiza tu navegador',
+                text: "Para disfrutar al máximo de nuestro sitio web y acceder a todas sus funciones, te recomendamos que actualices tu navegador web.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Entendido'
+            });
+        }
+    },
 };
 </script>
-  
