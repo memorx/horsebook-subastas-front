247e9d33f126a09c36ce5441fbfc77ea7a771788
Mrroboto9819 <pablo.cabrera.castrejon@gmail.com>
2023-10-31 17:11:08 -0600

    return nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -92,7 +92,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: 'http://localhost:8000/api'
-    baseURL: "https://subastas-api-preprod.alluxi.com/api",
+    baseURL: process.env.BASE_URL || "https://subastas-api-preprod.alluxi.com/api",
     baseLaSilla: "https://www.horsebooklasilla.com/api"
   },
 
@@ -101,9 +101,9 @@ export default {
   },
   publicRuntimeConfig: {
     // baseURL: 'http://localhost:8000/api'
-    apiToken: '0119158e9e647cc58e9c895fa08316b2a5b03df4',
-    baseURL: "https://subastas-api-preprod.alluxi.com/api",
-    baseURLWS: "wss://subastas-api-preprod.alluxi.com/ws",
+    apiToken: process.env.API_TOKEN || '0119158e9e647cc58e9c895fa08316b2a5b03df4',
+    baseURL: process.env.BASE_URL || "https://subastas-api-preprod.alluxi.com/api",
+    baseURLWS: process.env.BASE_URLWS || "wss://subastas-api-preprod.alluxi.com/ws",
     baseLaSilla: "https://www.horsebooklasilla.com/api"
   },
 
