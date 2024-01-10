ddf48a11a13626a74c2bd3235616bf0976c59ce9
Suzana Salcedo <suzana@alluxi.com>
2024-01-10 15:49:59 -0600

    News files

---
+++ pages/news/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-white w-full h-auto flex flex-col justify-start pt-4 px-6">
+       Noticias
+    </div>
+</template>
+<script>
+
+export default {
+    methods: {
+
+    }
+}
+</script>
